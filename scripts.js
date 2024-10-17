c2377c948323e928c5c1709dfd341ad3b57bb1c9
KING-ALEX <138088428+Cyber-Ghost-2008@users.noreply.github.com>
2024-10-17 07:17:34 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,8 @@
+function scrollToSecondPage() {
+    document.getElementById('second-page').scrollIntoView({ behavior: 'smooth' });
+}
+
+function scrollToLi() {
+    document.getElementById('scrolling').scrollIntoView({ behavior: 'smooth' });
+}
+
